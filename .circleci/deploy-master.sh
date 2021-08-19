c09fa9fb0f200e228cfb8d42fc4783e62d129377
LiangTao <admin@admin.com>
2021-08-19 16:31:13 +0800

    add dir

---
+++ .circleci/deploy-master.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+user=root
+host=118.193.61.10
+src=`pwd`"/"
+des=/data/123/
+now=`date +"%Y-%m-%d %H:%M:%S"`
+
+rsync -vzrc --delete  --exclude ".git"  --exclude ".env"   --exclude ".circleci"   $src  $user@$host:$des
+
+ssh $user@$host "sudo chown -R www:www $des"
+
+ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
+
+echo "$now update $host $des code"
