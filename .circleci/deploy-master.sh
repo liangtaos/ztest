aa4dcea0bce993bacde08232bde8ce1681a877bd
LiangTao <admin@admin.com>
2021-08-20 10:12:07 +0800

    820

--- .circleci/deploy-master.sh
+++ .circleci/deploy-master.sh
@@ -8,7 +8,7 @@ now=`date +"%Y-%m-%d %H:%M:%S"`
 
 rsync -vzrc --delete  --exclude ".git"  --exclude ".env"   --exclude ".circleci"   $src  $user@$host:$des
 
-ssh $user@$host "sudo chown -R www:www $des"
+#ssh $user@$host "sudo chown -R www:www $des"
 
 ssh $user@$host "chmod -R 775 $des/bootstrap/cache && chmod -R 775 $des/storage && cd $des && pwd && ls -al"
 
